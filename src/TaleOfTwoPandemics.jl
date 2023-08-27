de54e7ebe9741c1a516717f8455ff4b50df090e5
Anastasia Golovin <asgolovin@protonmail.com>
2023-08-27 09:31:02 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.40

---
+++ src/TaleOfTwoPandemics.jl
@@ -0,0 +1,5 @@
+module TaleOfTwoPandemics
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TaleOfTwoPandemics
+using Test
+
+@testset "TaleOfTwoPandemics.jl" begin
+    # Write your tests here.
+end
