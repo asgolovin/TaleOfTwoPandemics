af4a21ebe76794fabbff78bff8ba521231b4e37e
Anastasia Golovin <asgolovin@protonmail.com>
2023-08-28 19:57:01 +0200

    GUI MVP works!

--- src/visualization/gui.jl
+++ src/visualization/gui.jl
@@ -10,7 +10,8 @@ function gui(model)
         arrow_show=false, # hide directions of graph edges
         edge_color=:black, # change edge colors and widths with own functions
         edge_width=1,
-        edge_plottype=:linesegments # needed for tapered edge widths
+        edge_plottype=:linesegments, # needed for tapered edge widths
+        dimension=2,
     )
 
     fig, ax, abmobs = abmplot(model; (agent_step!)=agent_step!, (model_step!)=nothing,
