96dd0bccdaf0687b16b48e0594f0d3cf0a443a28
Anastasia Golovin <asgolovin@protonmail.com>
2023-08-31 11:26:36 +0200

    Add the learning update

--- src/simulation/knowledge.jl
+++ src/simulation/knowledge.jl
@@ -47,47 +47,14 @@ end
 
 function learning_update(agent, model)
   ΔQlearn = Dict(practice => 0.0 for practice in model.practices)
+  direction = sign(agent.payoff)
+  Qagent = agent.knowledge
+
+  for practice in model.practices
+    isactive = agent.strategy[practice] ? 1.0 : 0.0
+    ΔQlearn[practice] = direction * isactive * (1 - Qagent[practice]) * 0.5
+  end
   return ΔQlearn
-end
-
-function propagate_knowledge!(agent, other, model)
-  influence_strength = 0.3
-  repulsion_threshold = 10000 # no repulsion
-  minimum_difference = 0.3
-  total_diff = 0
-
-  for (state, action_space) in agent.knowledge
-    other_action_space = other.knowledge[state]
-    total_diff += sum(abs(action_space[a] - other_action_space[a]) for a in keys(action_space))
-  end
-
-  # Apply opinion dynamics using SJT rules based on the total difference
-  if total_diff < t && total_diff > minimum_difference
-    for (state, action_space) in agent.knowledge
-      other_action_space = other.knowledge[state]
-      for (action, value) in agent.knowledge[state]
-        other_value = other_action_space[action]
-        diff = abs(value - other_value)
-        if diff < repulsion_threshold
-          agent.knowledge[state][action] += (influence_strength * (other_value - value))
-          other.knowledge[state][action] += (influence_strength * (value - other_value))
-        end
-      end
-    end
-  elseif total_diff > t
-    for (state, action_space) in agent.knowledge
-      other_action_space = other.knowledge[state]
-      for (action, value) in agent.knowledge[state]
-        other_value = other_action_space[action]
-        diff = abs(value - other_value)
-        if diff < repulsion_threshold
-          agent.knowledge[state][action] += (influence_strength * (value - other_value))
-          other.knowledge[state][action] += (influence_strength * (other_value - value))
-        end
-      end
-    end
-  end
-  return agent, other
 end
 
 function fermi(x, β, mean)
