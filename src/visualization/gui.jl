2f35fdb5eef14be108a29af0745c2d85e48c049e
Anastasia Golovin <asgolovin@protonmail.com>
2023-08-31 12:36:59 +0200

    Visualize knowledge and strategies in the gui

--- src/visualization/gui.jl
+++ src/visualization/gui.jl
@@ -7,17 +7,62 @@ using Statistics
 
 export gui
 
-function agent_color(agents)
+function agent_marker(agents)
     for agent in agents
         if agent.status == S
-            return :green
+            return :circle
         elseif agent.status == I
+            return :star6
+        else
+            return :rect
+        end
+    end
+end
+
+# function agent_color(agents)
+#     for agent in agents
+#         if agent.status == S
+#             return :green
+#         elseif agent.status == I
+#             return :red
+#         elseif agent.status == R
+#             return :blue
+#         end
+#     end
+#     return RGB(0, 0, 0)
+# end
+
+function agent_color(agents)
+    practice = "handwashing"
+    for agent in agents
+        if agent.strategy[practice]
+            return :green
+        else
             return :red
-        elseif agent.status == R
-            return :blue
         end
     end
     return RGB(0, 0, 0)
+end
+
+function agent_size(agents)
+    practice = "handwashing"
+    for agent in agents
+        return 7 + agent.knowledge[practice] * 40
+    end
+end
+
+function edge_color(model)
+    w = []
+    for e in edges(model.space.graph)
+        agent1 = model[e.src]
+        agent2 = model[e.dst]
+        if distance(agent1, agent2) > model.similarity_threshold
+            push!(w, :lightgray)
+        else
+            push!(w, :black)
+        end
+    end
+    return w
 end
 
 function gui(model)
@@ -36,7 +81,7 @@ function gui(model)
         end)
 
         push!(alabels, practice)
-        push!(adata, (eval(Symbol(practice)), std))
+        push!(adata, (eval(Symbol(practice)), sum))
     end
 
     params = Dict(
@@ -46,13 +91,13 @@ function gui(model)
     graphplotkwargs = (
         layout=GraphMakie.Shell(), # node positions
         arrow_show=false, # hide directions of graph edges
-        edge_color=:black, # change edge colors and widths with own functions
+        edge_color=edge_color, # change edge colors and widths with own functions
         edge_width=1,
         edge_plottype=:linesegments, # needed for tapered edge widths
     )
 
     fig, abmobs = abmexploration(model;
-        (agent_step!)=agent_step!, ac=agent_color, params, graphplotkwargs,
+        (agent_step!)=agent_step!, ac=agent_color, as=agent_size, am=agent_marker, params, graphplotkwargs,
         adata, alabels)
 
     return fig
